56e03c52e58233a684c9d64fbe575d1e653c137c
DariaZhur <dariazhur89@gmail.com>
2024-07-03 15:31:43 +0300

    fix img name

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
+    console.log("DOM полностью загружен и разобран");
 
     const phoneInputField = document.querySelector("#phone");
     const phoneInput = window.intlTelInput(phoneInputField, {
         utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.13/js/utils.js",
         initialCountry: "ua"
     });
+    console.log("IntlTelInput инициализирован");
 
     document.getElementById('form').addEventListener('submit', function (event) {
         event.preventDefault();
+        console.log("Форма отправлена");
 
         const form = event.target;
         const name = form.name.value.trim();
@@ -62,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log("Форма данных для отправки:", formData);
 
         loader.style.display = 'block';
-        sendEmailToTelegram(formData);
+        // sendEmailToTelegram(formData);
         sendEmailToEmailJS(formData);
 
         form.reset();
@@ -74,7 +77,7 @@ document.addEventListener('DOMContentLoaded', function () {
     //     const url = 'https://api.telegram.org/bot7301777221:AAHN2KyML82ReOlE2BHpQhqU6TqyEW2L7m0/sendMessage';
     
     //     const message = `
-    //         новая регистрация:
+    //         Сообщение с формы:
     //         Страна: ${country}
     //         Имя: ${name}
     //         Email: ${email}
@@ -86,6 +89,7 @@ document.addEventListener('DOMContentLoaded', function () {
     //         text: message,
     //     });
     
+    //     // Отправляем запрос
     //     fetch(`${url}?${params}`, { method: 'GET' })
     //         .then(response => {
     //             if (!response.ok) {
@@ -115,10 +119,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
         emailjs.send('service_au6eag6', 'template_ywg1g9x', templateParams)
             .then(function (response) {
+                console.log("Email успешно отправлен через EmailJS", response.status, response.text);
                 alert('Ваши данные успешно отправлены!');
                 loader.style.display = 'none';
             })
             .catch(function (error) {
+                console.error("Ошибка при отправке email через EmailJS:", error);
                 alert('Ошибка! Данные не отправлены!');
                 loader.style.display = 'none';
             });
